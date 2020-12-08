42556a862f2c1aa29acaf993887b7b244dd96285
Raul Ranete <raul.ranete@gmail.com>
2020-12-09 01:18:45 +0200

    Update ormconfig.ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,16 +1,21 @@
 module.exports = {
     name: 'default',
     type: 'postgres',
-    host: 'localhost',
+    host: 'ec2-54-228-250-82.eu-west-1.compute.amazonaws.com',
     port: 5432,
-    username: 'postgres',
-    password: 'Serpentina',
-    database: 'ehsfocus',
+    username: 'yhgbefvpubaszl',
+    password: '615ec815f556a28aa918b6f5e1cc1514d018b5a371637fcf06e4baef4b602ea9',
+    database: 'd710o49bke68if',
     synchronize: false,
     dropSchema: false,
-    logging: true,
+    logging: false,
     entities:  ["dist/**/*.entity{.ts,.js}"],
   };
 
-
+//     DB_HOST : 'ec2-54-228-250-82.eu-west-1.compute.amazonaws.com',
+//     DB_PASSWORD : '615ec815f556a28aa918b6f5e1cc1514d018b5a371637fcf06e4baef4b602ea9',
+//     DB_DATABASE:'d710o49bke68if',
+//     DB_NAME:  'default',
+//     DB_USER: 'yhgbefvpubaszl',
+//     DB_PORT :5432,
   
